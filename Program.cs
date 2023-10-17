8e4224a950138ff4d8bafda6b765c533de27b9c1
narramuni <nmspavan@gmail.com>
2023-10-17 15:44:53 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueAssist
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            runApp();
+        }
+        public static void runApp()
+        {
+            Queues Q = new Queues(10);
+
+            Q.insert(1);
+            Q.insert(2);
+            Q.insert(3);
+            Q.insert(4);
+            Q.insert(5);          
+            Console.WriteLine(Q.printQueue());
+
+            Q.delete();
+            Q.delete();
+
+            Console.WriteLine(Q.printQueue());
+
+            Console.ReadLine(); 
+        }
+
+    }
+}
+
---
+++ Queues.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QueueAssist
+{
+    public class Queues
+    {
+        private int[] element;
+        private int front;
+        private int rear;
+        private int max;
+
+        public Queues(int size)
+        {
+            element = new int[size];
+            front = 0;
+            rear = -1;
+            max = size;
+        }
+
+        public void insert(int item)
+        {
+            if (rear == max - 1)
+            {
+                return;
+            }
+            else
+            {
+                element[++rear] = item;
+            }
+        }
+
+        public int delete()
+        {
+            if (front == rear + 1)
+            {
+                return -1;
+            }
+            else
+            {
+                return element[front++];
+            }
+        }
+
+        public string printQueue()
+        {
+            string retVal = "";
+            if (front == rear + 1)
+            {
+                return retVal;
+            }
+            else
+            {
+                for (int i = front; i <= rear; i++)
+                {
+                    retVal += element[i] + " ";
+                }
+            }
+            return retVal;
+        }
+    }
+}
+
+
